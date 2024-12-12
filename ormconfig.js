962e0d0f12cad271a003bb70eb4e0387c5169d01
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2024-12-12 14:40:00 +0400

    finished app

---
+++ ormconfig.js
@@ -0,0 +1,41 @@
+var dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.js'],
+      migrationsRun: true,
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    throw new Error('unknow environment');
+}
+
+module.exports = dbConfig;
